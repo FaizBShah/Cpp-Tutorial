d783d0cd3d0fa7b8ac6172858d32bdcf0c9616ba
FaizBShah <faizbshah2001@gmail.com>
2024-01-03 19:32:03 +0530

    Different type of initializations

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World" << std::endl;
+    int a;
+    int b = 1;
+    int c(2);
+    int d { 4 }; // C++ 11 way of doing. Preferred.
+    int e = { 5 };
+    int f{};
+
+    std::cout << a << " " << b << " " << c << " " << d << " " << e << " " << f;
+
     return 0;
 }
