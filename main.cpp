f6455a0cf27210fbb7d3d2585e18e1c63ec76598
FaizBShah <faizbshah2001@gmail.com>
2024-01-13 00:23:24 +0530

    Forward declaration

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 
-void doPrint() {
-    std::cout << "In doPrint()\n";
-}
+// Doing forward declaration so that function can be used even if defined out of order
+void doPrint();
 
 int main() {
     std::cout << "Starting main()\n";
     doPrint();
     std::cout << "Ending main()\n";
+}
+
+void doPrint() {
+    std::cout << "In doPrint()\n";
 }
