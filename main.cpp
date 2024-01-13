96b4138562ee7e7ed989d1a3f99928cc5d011de1
FaizBShah <faizbshah2001@gmail.com>
2024-01-14 00:14:12 +0530

    Added support for multiple files

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 
-// Doing forward declaration so that function can be used even if defined out of order
-void doPrint();
+// In forward declarations, parameter names are optional
+// This forward declaration is important in multi-file codes
+// so that this file (i.e. main.cpp) can compile. The add() declaration
+// gets linked to the add.cpp implementation during the linking stage
+// by the linker
+int add(int, int);
 
 int main() {
-    std::cout << "Starting main()\n";
-    doPrint();
-    std::cout << "Ending main()\n";
-}
-
-void doPrint() {
-    std::cout << "In doPrint()\n";
+    std::cout << "The sum of 3 + 4 is " << add(3, 4) << '\n';
 }
