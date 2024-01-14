2d842751cbc77e9ef115950ffd89521f1663ba14
FaizBShah <faizbshah2001@gmail.com>
2024-01-14 22:12:35 +0530

    preprocessor directives

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 
-// This is a macro, and it will replace all occurence
-// of the first text with the second
-#define NAME "Faiz"
+#define PRINT_JOE
 
 int main() {
-    std::cout << "My name is " << NAME << '\n';
+    #ifdef PRINT_JOE
+        std::cout << "This statement will get compiled, because PRINT_JOE macro is defined\n";
+    #endif
+
+    // This is equivalent to the above statement
+    #if defined(PRINT_JOE)
+        std::cout << "This statement will get compiled, because PRINT_JOE macro is defined\n";
+    #endif
+
+    #ifdef PRINT_BOB
+        std::cout << "This statement will not get compiled, because PRINT_BOB macro is not defined\n";
+    #endif
+
+    #ifndef PRINT_BOB
+        std::cout << "This statement will get compiled, because PRINT_BOB macro is not defined\n";
+    #endif
+
+    // This is equivalent to the above statement
+    #if !defined(PRINT_BOB)
+        std::cout << "This statement will get compiled, because PRINT_BOB macro is not defined\n";
+    #endif
+
+    #if 0
+        std::cout << "This statement will not get compiled, because #if 0 is the condition\n";
+    #endif
+
+    #if 1
+        std::cout << "This statement will get compiled, because #if 1 is the condition\n";
+    #endif
+
+    #if 2
+        std::cout << "This statement will also get compiled, because #if 2 is the condition\n";
+    #endif
 }
