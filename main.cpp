146b5ece3d925565c94222893f78648e05c5cb6b
FaizBShah <faizbshah2001@gmail.com>
2024-01-14 21:57:55 +0530

    Macro with text-substitution

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 
-// In forward declarations, parameter names are optional
-// This forward declaration is important in multi-file codes
-// so that this file (i.e. main.cpp) can compile. The add() declaration
-// gets linked to the add.cpp implementation during the linking stage
-// by the linker
-int add(int, int);
+// This is a macro, and it will replace all occurence
+// of the first text with the second
+#define NAME "Faiz"
 
 int main() {
-    std::cout << "The sum of 3 + 4 is " << add(3, 4) << '\n';
+    std::cout << "My name is " << NAME << '\n';
 }
