d293731ebb2a706a23ede0fc4c102365ce65e1fb
FaizBShah <faizbshah2001@gmail.com>
2024-01-21 16:27:30 +0530

    cerr debugging

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 #include <iostream>
 
 int main() {
+    std::cerr << "Debugging\n"; // Use for printing debugging statements. cerr is unbuffered, unlike cout, and hence better
     std::cout << getSquareSides() << '\n';
 }
