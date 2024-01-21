fcbce67a28cfdd92d5c79c60bc82273519a73429
FaizBShah <faizbshah2001@gmail.com>
2024-01-21 18:37:15 +0530

    Used plog for logging

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <plog/Log.h>
+#include <plog/Initializers/RollingFileInitializer.h>
+
+int getUserInput() {
+    PLOGD << "In function getUserInput()";
+
+    std::cout << "Enter a number\n";
+
+    int x {};
+    std::cin >> x;
+
+    return x;
+}
 
 int main() {
-    
+    plog::init(plog::debug, "LogFile.txt"); // Initializing the logger library
+
+    PLOGD << "In function main()"; // PLOGD is used to log in debug level
+
+    int x { getUserInput() };
+    std::cout << "You entered: " << x << '\n';
 }
