d1befe0e3bffcc2ad8b146d27f54f5924bcd2ffe
FaizBShah <faizbshah2001@gmail.com>
2024-01-21 16:44:50 +0530

    Added external c++ library plog

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
-#include "square.h"
-#include "wave.h"
 #include <iostream>
 
 int main() {
-    std::cerr << "Debugging\n"; // Use for printing debugging statements. cerr is unbuffered, unlike cout, and hence better
-    std::cout << getSquareSides() << '\n';
+    
 }
--- square.h
+++
@@ -1,9 +0,0 @@
-// #pragma once is another way to create header guard
-// Warning: Its a new feature, so not all compilers support it
-// Also, the implementation of #pragma directives are different for
-// different compilers. Not all #pragma commands run for all compilers
-#pragma once
-
-int getSquareSides() {
-    return 4;
-}
