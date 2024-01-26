48c1e87a607af9df33f5e6737f65a609cbea99d4
FaizBShah <faizbshah2001@gmail.com>
2024-01-27 00:08:27 +0530

    Added warning note about std::int8_t and std::uint8_t

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 // Least types with size T are types that are the smallest size with minimum size atleast T
 // Using fast and least types is not used that much as since the size changes with different
 // architectures, the result might be inconsistent and unexpected
+
+// Note:- Most compilers treat std::int8_t and std::unit8_t and their corresponding fast and
+// least types as chars due to an oversight in the C++ specifications, and thus they should be avoided
 int main() {
     std::cout << "least 8:  " << sizeof(std::int_least8_t) * 8 << " bits\n";
 	std::cout << "least 16: " << sizeof(std::int_least16_t) * 8 << " bits\n";
