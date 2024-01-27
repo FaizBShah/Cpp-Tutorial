029cf76cb7dec37682a188e8523c9991a96b5885
FaizBShah <faizbshah2001@gmail.com>
2024-01-27 20:00:53 +0530

    Printing of floating numbers

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    float fValue{0.04f};
-    double dValue{3.14159};
-    long double ldValue{1.1111111111};
+    std::cout << 5.0 << '\n';         // Will print 5
+    std::cout << 6.7f << '\n';        // Will print 6.7
+    std::cout << 9876543.21 << '\n';  // Will print 9.87654e+06
 }
