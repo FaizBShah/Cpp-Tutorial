d97a64351720b4c1d281fc33c7697f6a63e94847
FaizBShah <faizbshah2001@gmail.com>
2024-03-08 00:26:17 +0530

    constexpr variables

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+int getVal() {
+    return 5;
+}
+
 int main() {
-    const int x { 3 + 4 };  // constant expression 3 + 4 must be evaluated at compile-time by the compiler
-    int y { 3 + 4 };        // constant expression 3 + 4 may be evaluated at compile-time or runtime by the compiler
+    // constexpr keyword is a keyword which defines that a variable is a constant + is initialized by a value which
+    // is known at compile time (i.e. a constant expression).
+    // All constexpr variables are implicitly const, but not all const variables are implicitly constexpr.
+    // Function parameters can be const, but cannot be constexpr because the parameter's values are decided
+    // during run-time, not compile-time.
+    // There are special functions which do support constexpr parameters, called consteval functions.
+    constexpr double g { 9.8 };  // This will compile
+    // constexpr int val { getVal() }; // This will not compile
 }
