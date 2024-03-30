db65daad790e7d432784645247a6220c8432d714
FaizBShah <faizbshah2001@gmail.com>
2024-03-30 17:01:45 +0530

    Using best of both consteval and constexpr

--- main.cpp
+++ main.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 
-// For constexpr functions, unless the value returned
-// by the function is not being used in a constant expression
-// environment (such as being assigned to a constexpr variable),
-// then the function has no guarantee to be evaluated during compile-time.
-// consteval keyword ensures that the function is always evaluated during
-// compile-time if the parameters are constexpr, no matter what. And if
-// for some reason compile-time evaluation is not possible (for example,
-// the parameter being passed is not constexpr), then it'll throw an error
-// The disadvantage of consteval functions are that unlike constexpr, consteval
-// functions never evaluate during run-time.
-consteval int greater(int x, int y) {
+// Since one of the main disadvantages of consteval functions
+// is that it cannot be used during run-time evaluations, thus
+// we can use the best of both consteval and constexpr by creating
+// this below consteval helper function. Whenever we want to evaluate
+// a constexpr function during compile-time, we can wrap it inside this
+// helper function. And whenever we the constexpr function during compile-time,
+// we can just call it directly.
+consteval auto compileTime(auto value) {
+    return value;
+}
+
+constexpr int greater(int x, int y) {
     return x > y ? x : y;
 }
 
 int main() {
-    const int g { greater(5, 6) };  // Will be evaluated during compile-time
-    std::cout << g << '\n';
-
-    std::cout << greater(5, 6) << '\n';  // Will be evaluated during compile-time. (If greater() was constexpr, it might or might not get evaluated during compile-time)
+    std::cout << greater(5, 6) << '\n';               // This function might or might not be evaluated during compile-time
+    std::cout << compileTime(greater(5, 6)) << '\n';  // This function will always be evaluated during compile-time, as its wrapped by a consteval function
 
     int x { 5 };
-    // std::cout << greater(x, 6) << '\n'; This will throw compile-time error as x is not constexpr variable, hence this function cannot be evaluated during compile-time
+
+    std::cout << greater(x, 6) << '\n';  // We can still use the function for during run-time evaluations
 }
