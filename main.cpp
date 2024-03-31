aa59f4d12ed81b5eac881fee8d6c371e33b2962f
FaizBShah <faizbshah2001@gmail.com>
2024-03-31 21:39:44 +0530

    Taking string inputs using std::getline()

--- main.cpp
+++ main.cpp
@@ -5,11 +5,10 @@ int main() {
     std::string name {};
     std::string color {};
 
-    // If you enter Faiz Shah and Red, then name will contain Faiz, and color will contain Shah
-    // Thus, cin does not work with std::string, as it only inputs word till the space character
-    // is occured.
-    std::cin >> name;
-    std::cin >> color;
+    // To input string properly, use std::getline() instead of std::cin
+    // The std::ws is an input manipulator which ignores leading whitespaces in input
+    std::getline(std::cin >> std::ws, name);
+    std::getline(std::cin >> std::ws, color);
 
-    std::cout << "The name is: " << name << " and color is: " << color << '\n';  // Will print Faiz and Shah, instead of Faiz Shah and Red
+    std::cout << "The name is: " << name << " and color is: " << color << '\n';  // Will print Faiz Shah and Red properly
 }
