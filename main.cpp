fe1f7ec05635b497982f24de81449eaaf93c595d
FaizBShah <faizbshah2001@gmail.com>
2024-03-31 21:31:18 +0530

    string input doesn't work with std::cin

--- main.cpp
+++ main.cpp
@@ -2,11 +2,14 @@
 #include <string>
 
 int main() {
-    std::string name1 { "Jason" };  // String with value
-    name1 = "Faiz";                 // The variable can be re-assigned, and that too to a string of different length
+    std::string name {};
+    std::string color {};
 
-    std::string name2 {};  // Empty string
+    // If you enter Faiz Shah and Red, then name will contain Faiz, and color will contain Shah
+    // Thus, cin does not work with std::string, as it only inputs word till the space character
+    // is occured.
+    std::cin >> name;
+    std::cin >> color;
 
-    std::cout << "My name is: " << name1 << '\n';  // You can use string variables directly in std::cout
-    std::cout << '[' << name2 << ']' << '\n';      // This will print []
+    std::cout << "The name is: " << name << " and color is: " << color << '\n';  // Will print Faiz and Shah, instead of Faiz Shah and Red
 }
