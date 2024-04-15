5ca6e6f76a0a7251008b64c6ce602470b0c399f1
FaizBShah <faizbshah2001@gmail.com>
2024-04-15 22:57:08 +0530

    string_view

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <string>
+#include <string_view>
 
+void print(std::string_view s) {
+    std::cout << s << '\n';
+}
+
+// Its expensive to initialize and copy a string, therefore string_view
+// was introduced in C++17. string_view is a read-only copy of an existing
+// string, and is much performant and faster in initializing and copying.
 int main() {
-    // Using std::string with constexpr is not supported in C++17 and before,
-    // and only limite support in C++20/23, so avoid using it
-    // constexpr std::string name { "Faiz" };
+    std::string_view s { "Hello World" };
+    print(s);
+
+    std::string s1 { "Hello World 2" };  // std::string implicitly gets converted to std::string_view
+    print(s1);
 }
