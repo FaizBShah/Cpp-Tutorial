227babf695342d6f5931bffd9c0e4ae0dd509c5c
FaizBShah <faizbshah2001@gmail.com>
2024-07-20 00:23:30 +0530

    Rules regarding returning a string_view

--- main.cpp
+++ main.cpp
@@ -2,24 +2,33 @@
 #include <string>
 #include <string_view>
 
-std::string getName() {
-    std::string s { "Faiz" };
-    return s;
+// This is unsafe return since t and f, the objects which the
+// returned string_view views, is destroyed as soon as this function is over.
+// Hence, creating a dangling string_view
+std::string_view getBoolName1(bool b) {
+    std::string t { "true" };
+    std::string f { "false" };
+
+    return b ? t : f;
 }
 
-// Incorrect usages of string_view
+// This is safe because the C-style strings "true" and "false"
+// remains throughout the lifetime of the program
+std::string_view getBoolName2(bool b) {
+    return b ? "true" : "false";
+}
+
+// This is also safe if the arguments passed to this function exists even after
+// the function ends (since we are returning the views of the arguments only)
+// If an argument is a temporary that is destroyed at the end of the full expression
+// containing the function call, the returned std::string_view must be used immediately,
+// as it will be left dangling after the temporary is destroyed.
+std::string_view checkAlphabetical(std::string_view s1, std::string_view s2) {
+    return s1 < s2 ? s1 : s2;
+}
+
 int main() {
-    std::string_view sv {};  // Initialized a string_view object
-
-    {
-        std::string s { "Hello World" };
-        sv = s;  // s will get destroyed after this line, as it doesn't exist outside of this block
-    }
-
-    // This will give undefined behaviour because the object which the string_view was viewing (s) is already destryed and doesn't exist
-    std::cout << sv << '\n';
-
-    // This will also give undefined behavior since the "s" object being returned from the function gets destroyed once the function is executed
-    std::string_view namev { getName() };
-    std::cout << namev << '\n';
+    std::cout << getBoolName1(true) << " " << getBoolName1(false) << '\n';
+    std::cout << getBoolName2(true) << " " << getBoolName2(false) << '\n';
+    std::cout << checkAlphabetical("Hello", "World") << '\n';
 }
