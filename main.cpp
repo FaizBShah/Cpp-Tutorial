a9bafbe2519755de219db68e10ee37a5287162fa
FaizBShah <faizbshah2001@gmail.com>
2024-07-20 00:35:41 +0530

    More notes on string_view

--- main.cpp
+++ main.cpp
@@ -2,33 +2,19 @@
 #include <string>
 #include <string_view>
 
-// This is unsafe return since t and f, the objects which the
-// returned string_view views, is destroyed as soon as this function is over.
-// Hence, creating a dangling string_view
-std::string_view getBoolName1(bool b) {
-    std::string t { "true" };
-    std::string f { "false" };
-
-    return b ? t : f;
-}
-
-// This is safe because the C-style strings "true" and "false"
-// remains throughout the lifetime of the program
-std::string_view getBoolName2(bool b) {
-    return b ? "true" : "false";
-}
-
-// This is also safe if the arguments passed to this function exists even after
-// the function ends (since we are returning the views of the arguments only)
-// If an argument is a temporary that is destroyed at the end of the full expression
-// containing the function call, the returned std::string_view must be used immediately,
-// as it will be left dangling after the temporary is destroyed.
-std::string_view checkAlphabetical(std::string_view s1, std::string_view s2) {
-    return s1 < s2 ? s1 : s2;
-}
-
+// Once the prefix or suffix is removed, it cannot be changed
+// back until the string_view is reassigned again. This feature
+// of string_view is also helpful to handle substrings. But those
+// substrings does not necessarilly terminate with null character.
+// But string_view still takes cares of the length function of the substring.
 int main() {
-    std::cout << getBoolName1(true) << " " << getBoolName1(false) << '\n';
-    std::cout << getBoolName2(true) << " " << getBoolName2(false) << '\n';
-    std::cout << checkAlphabetical("Hello", "World") << '\n';
+    std::string_view sv { "Peach" };
+
+    // Will convert sv to "each"
+    sv.remove_prefix(1);
+    std::cout << sv << '\n';
+
+    // Will convert sv to "ea"
+    sv.remove_suffix(2);
+    std::cout << sv << '\n';
 }
